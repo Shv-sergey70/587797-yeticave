c631242543aa3438a0fa7728836b324f4eda0a92
Shershnev Sergey <shv.sergey70@gmail.com>
2018-09-25 11:37:39 +0300

    module3-task1 - v2

--- templates/index.php
+++ templates/index.php
@@ -19,11 +19,11 @@
         <?foreach($catalog_items as $value):?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=htmlspecialchars($value['URL'])?>" width="350" height="260" alt="">
+                    <img src="<?=htmlspecialchars($value['URL'], ENT_QUOTES)?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=htmlspecialchars($value['CATEGORY'])?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($value['NAME'])?></a></h3>
+                    <span class="lot__category"><?=htmlspecialchars($value['CATEGORY'], ENT_QUOTES)?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($value['NAME'], ENT_QUOTES)?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
