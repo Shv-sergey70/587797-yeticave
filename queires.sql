7c25bea8845cb85ca0796aa87fb83b37fc369737
Shershnev Sergey <shv.sergey70@gmail.com>
2018-09-28 09:57:27 +0300

    module4-task2 v4

--- queires.sql
+++ queires.sql
@@ -12,13 +12,13 @@ INSERT INTO users (id, date_register, email, name, password, contacts) VALUES
 (2, '2018-09-21 00:00:00', 'alexey@gmail.com', 'Alexey', '123456', 'vk - id12322'), 
 (3, '2018-09-24 00:00:00', 'petrovich@gmail.com', 'Petr', '1234567', 'vk - id12321');
 
-INSERT INTO lots (name, adv_category_id, start_price, image_url, date_create, bet_step, author_id) VALUES 
-('2014 Rossignol District Snowboard', 1, 10999, 'img/lot-1.jpg', '2018-09-23 00:00:00', 100, 2),
-('DC Ply Mens 2016/2017 Snowboard', 1, 159999, 'img/lot-2.jpg', '2018-09-22 00:00:00', 200, 3), 
-('Крепления Union Contact Pro 2015 года размер L/XL', 2, 8000, 'img/lot-3.jpg', '2018-09-21 00:00:00', 400, 1),
-('Ботинки для сноуборда DC Mutiny Charocal', 3, 10999, 'img/lot-4.jpg', '2018-09-18 00:00:00', 500, 1), 
-('Куртка для сноуборда DC Mutiny Charocal', 4, 7500, 'img/lot-5.jpg', '2018-09-25 00:00:00', 800, 2), 
-('Маска Oakley Canopy', 6, 5400, 'img/lot-6.jpg', '2018-09-22 00:00:00', 450, 3);
+INSERT INTO lots (name, adv_category_id, start_price, date_end, image_url, date_create, bet_step, author_id) VALUES 
+('2014 Rossignol District Snowboard', 1, 10999, '2018-09-30 00:00:00', 'img/lot-1.jpg', '2018-09-23 00:00:00', 100, 2),
+('DC Ply Mens 2016/2017 Snowboard', 1, 159999, '2018-10-10 00:00:00', 'img/lot-2.jpg', '2018-09-22 00:00:00', 200, 3), 
+('Крепления Union Contact Pro 2015 года размер L/XL', 2, 8000, '2018-10-02 00:00:00', 'img/lot-3.jpg', '2018-09-21 00:00:00', 400, 1),
+('Ботинки для сноуборда DC Mutiny Charocal', 3, 10999, '2018-10-02 00:00:00', 'img/lot-4.jpg', '2018-09-18 00:00:00', 500, 1), 
+('Куртка для сноуборда DC Mutiny Charocal', 4, 7500, '2018-10-01 00:00:00', 'img/lot-5.jpg', '2018-09-25 00:00:00', 800, 2), 
+('Маска Oakley Canopy', 6, 5400, '2018-10-21 00:00:00', 'img/lot-6.jpg', '2018-09-22 00:00:00', 450, 3);
 
 INSERT INTO bets SET date_create = '2018-09-20 00:00:00', price = 4300, user_id = 1, lot_id = 3;
 INSERT INTO bets SET date_create = '2018-09-21 00:00:00', price = 2800, user_id = 3, lot_id = 5;
@@ -33,7 +33,8 @@ INSERT INTO bets SET date_create = '2018-09-24 00:00:00', price = 10000, user_id
 -- lots.name AS lot_name, 
 -- lots.start_price AS lot_start_price, 
 -- lots.image_url,
--- lots.date_create, 
+-- lots.date_create,
+-- lots.date_end, 
 -- lots.bet_step, 
 -- COUNT(bets.lot_id) AS bets_number, 
 -- categories.name AS category_name 
@@ -42,7 +43,7 @@ INSERT INTO bets SET date_create = '2018-09-24 00:00:00', price = 10000, user_id
 -- ON lots.id = bets.lot_id 
 -- JOIN categories 
 -- ON lots.adv_category_id = categories.id 
--- WHERE lots.date_create > CURDATE()
+-- WHERE lots.date_end > CURDATE()
 -- GROUP BY bets.lot_id
 -- ORDER BY lots.date_create DESC
 
